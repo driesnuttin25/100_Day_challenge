5b7a87baa55a41dd3cca12f5acf783eb9f2401d4
Dries Nuttin <dries.nuttin@hotmail.com>
2023-10-29 16:23:10 +0100

    Day_3_code_evaluation

--- Code_100_days/Day_3/Day_3.cpp
+++ Code_100_days/Day_3/Day_3.cpp
@@ -1,41 +1,39 @@
-#include <stdio.h>
 #include <iostream>
+#include <vector>
 using namespace std;
 
 int main()
 {
-    long long fib[200000];
+    vector<long long> fib;
     int amount;
-    fib[0] = 1;
-    fib [1] = 1;
-    string yon;
-    cout << "Please enter the amount of fibobacci numbers you want: " <<endl;
+    string printReverse;
+    cout << "Please enter the number of Fibonacci terms you want: ";
     cin >> amount;
-    cout << "Would you like it to be printed backwards? 'y' or 'n'";
-    cin >> yon;
-    cout << endl << "Printing Fibonacci sequence: " << endl;
+    cout << "Would you like it to be printed backwards? 'y' or 'n': ";
+    cin >> printReverse;
 
-    if(amount < 1){
-        cout <<endl << "Please enter a real amount" << endl;
-    }
-    else
-    {
-
-    for(int i = 2; i < amount; i++){
-        fib[i] = fib[i-1] + fib[i-2];
+    if (amount < 1) {
+        cout << "Please enter a valid amount." << endl;
+        return 0;
     }
 
-    if(yon == "y"){
-        for(int j = amount -1;  j >= 0; j--){
-        cout << fib[j] << endl;
+    fib.push_back(1);
+    fib.push_back(1);
+
+    for (int i = 2; i < amount; ++i) {
+        fib.push_back(fib[i - 1] + fib[i - 2]);
     }
 
+    cout << "Printing Fibonacci sequence:" << endl;
+    if (printReverse == "y") {
+        for (int i = amount - 1; i >= 0; --i) {
+            cout << fib[i] << endl;
+        }
+    } else {
+        for (const auto &num : fib) {
+            cout << num << endl;
+        }
     }
-    else{
-    for(int j = 0;  j < amount; j++){
-        cout << fib[j] << endl;
-    }
-    }
-    }
+
     return 0;
 }
