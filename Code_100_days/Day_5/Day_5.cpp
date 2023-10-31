e45869915c64852a22d99b2ab1b080e56e14f87b
Dries Nuttin <dries.nuttin@hotmail.com>
2023-10-31 20:17:54 +0100

    Added more modularity to code

--- Code_100_days/Day_5/Day_5.cpp
+++ Code_100_days/Day_5/Day_5.cpp
@@ -4,71 +4,82 @@
 
 using namespace std;
 
-int main(){
-    string option;
-    while(true){
-    cout << "Please select an option" << endl << "- Create" << endl << "- Write" << endl << "- Read" << endl << "- Append" << endl << "- Exit" << endl << "Option = ";
-    cin >> option;
-    if(option == "create"){
-        //create();
-        string name;
-        cout << "Please select a .txtfile name: ";
-        cin >> name;
-        ofstream file(name);
-        cout << endl << "Created file" << endl;
-
+void create(){
+    string name;
+    cout << "Please select a .txtfile name: ";
+    cin >> name;
+    ofstream file(name);
+    cout << endl << "Created file" << endl;
+};
+void write(){
+    string name1;
+    char text[300];
+    cout << "To which file would you like to write?: ";
+    cin >> name1;
+    ofstream file(name1);
+    if(!file.is_open()){
+        cout << "No such file in directory.";
     }
-    else if(option == "write"){
-        string name1;
-        char text[300];
-        cout << "To which file would you like to write?: ";
-        cin >> name1;
-        ofstream file(name1);
-        if(!file.is_open()){
-            cout << "No such file in directory.";
-        }
-        else{
+    else{
         cout << "Writing in file:" << endl;
         cin.ignore();
         cin.getline(text, sizeof(text));
         file << text;
         file.close();
-        }
     }
-    else if(option == "read"){
-        string name2;
-        string text1;
-        cout << "Which file would you like to read?: ";
-        cin >> name2;
-        ifstream file(name2);
-        if(!file.is_open()){
+};
+void read(){
+    string name2;
+    string text1;
+    cout << "Which file would you like to read?: ";
+    cin >> name2;
+    ifstream file(name2);
+    if(!file.is_open()){
         cout << "No such file in directory.";
-        }
-        else{
+    }
+    else{
         cout << "Reading in file:" << endl;
         string text;
         text.assign((istreambuf_iterator<char>(file)),
                     (istreambuf_iterator<char>()));
         cout << text << endl;
         file.close();
-        }
     }
-    else if(option == "append"){
-        string name3;
-        char text[300];
-        cout << "To which file would you like to append?: ";
-        cin >> name3;
-        ofstream file(name3, ios::app);
-        if(!file.is_open()){
+};
+void append(){
+    string name3;
+    char text[300];
+    cout << "To which file would you like to append?: ";
+    cin >> name3;
+    ofstream file(name3, ios::app);
+    if(!file.is_open()){
         cout << "No such file in directory.";
-        }
-        else{
+    }
+    else{
         cout << "Appending to file:" << endl;
         cin.ignore();
         cin.getline(text, sizeof(text));
         file << text << endl;
         file.close();
-        }
+    }
+};
+
+int main(){
+    string option;
+    while(true){
+    cout << "Please select an option" << endl << "- Create" << endl << "- Write" << endl << "- Read" << endl << "- Append" << endl << "- Exit" << endl << "Option = ";
+    cin >> option;
+    if(option == "create"){
+        create();
+    }
+    else if(option == "write"){
+        write();
+    }
+    else if(option == "read"){
+        read();
+    }
+    else if(option == "append"){
+        append();
     }
     else if(option == "exit"){
         cout <<"Exiting....." << endl;
